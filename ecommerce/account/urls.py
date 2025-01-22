98be11c80c7092dbb0186469a8a88dd3ca39d28a
Angelina <angelinachigrinets@gmail.com>
2025-01-22 17:29:21 +0100

    implement password reset

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render
 from . import views as v
 
@@ -15,5 +16,24 @@ urlpatterns = [
     path('profile-management', v.manage_profile, name='manage_profile'),
     path('delete-account', v.delete_user, name='delete_account'),
 
+# Password reset - extending Django's auth standard class-based views
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/password/password_reset.html',
+        email_template_name='account/password/password_reset_email.html',
+        success_url=reverse_lazy('account:password_reset_done')),
+        name='password_reset'),
+    
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='account/password/password_reset_done.html'),
+        name='password_reset_done'),
+    
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='account/password/password_reset_confirm.html',
+        success_url=reverse_lazy('account:password_reset_complete')),
+        name='password_reset_confirm'),
+    
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='account/password/password_reset_complete.html'),
+        name='password_reset_complete'),
     
 ]
